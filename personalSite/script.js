f3eafc4f5f5ad9440b2c7e6a51fcfce83fe22f2d
TheGhossst <nandukrishnapkm97@gmail.com>
2024-08-17 19:12:18 +0530

    update: navigation link and script to toggle flip card on click

--- personalSite/script.js
+++ personalSite/script.js
@@ -1,4 +1,4 @@
-document.querySelector('.nav-bar-item a[href=".profile"]').addEventListener('click', function(event) {
+document.querySelector('.nav-bar-item a[href="turn"]').addEventListener('click', function(event) {
     event.preventDefault();
     document.querySelector('.flip-card').classList.toggle('flipped');
 });
