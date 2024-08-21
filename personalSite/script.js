7c8d5b85fa89c3192c57f4db3c95d764812d3195
TheGhossst <nandukrishnapkm97@gmail.com>
2024-08-21 18:49:05 +0530

    add:  navigation bar with new 'Bucket List' item, link to resume with confirmation prompt, and modified 'About me' link behavior.

--- personalSite/script.js
+++ personalSite/script.js
@@ -2,3 +2,7 @@ document.querySelector('.nav-bar-item a[href="turn"]').addEventListener('click',
     event.preventDefault();
     document.querySelector('.flip-card').classList.toggle('flipped');
 });
+
+function confirmLeaving() {
+    return confirm("You are leaving the page to view the resume. Do you want to proceed?");
+  }
